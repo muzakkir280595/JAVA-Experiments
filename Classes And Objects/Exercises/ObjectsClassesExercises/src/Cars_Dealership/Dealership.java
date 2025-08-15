86c38061da19f447c659e12a15112c4659de53bf
Muhammad Muzakkir Bin Dzulkarnain <muzakkircareer@gmail.com>
2025-08-15 11:55:44 +0800

    Remake The Object Classes First Exercises

---
+++ Classes And Objects/Exercises/ObjectsClassesExercises/src/Cars_Dealership/Dealership.java
@@ -0,0 +1,25 @@
+package Cars_Dealership;
+
+//Dealership.java
+public class Dealership {
+
+	public static void main (String[] args) {
+		//Creating the first Car object.
+		Car myCar = new Car();
+		myCar.brand = "Toyota";
+		myCar.model = "Camry";
+		myCar.year = 2022;
+		
+		System.out.println("My first car is a "+myCar.brand+""+myCar.model);
+		myCar.startEngine();
+		
+		//Creating the second Car Objects.
+		Car newCar = new Car();
+		newCar.brand = "Honda";
+		newCar.model = "Civic";
+		newCar.year = 2024;
+		
+		System.out.println("My second car is a "+myCar.brand+""+myCar.model);
+		newCar.drive();
+	}
+}
