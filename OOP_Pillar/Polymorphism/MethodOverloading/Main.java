952e525147119b8ba070f6189abcd3d554025066
Muhammad Muzakkir Bin Dzulkarnain <muzakkircareer@gmail.com>
2025-08-21 19:07:29 +0800

    Polymorphism Relearn

--- OOP_Pillar/Polymorphism/MethodOverloading/Main.java
+++ OOP_Pillar/Polymorphism/MethodOverloading/Main.java
@@ -15,7 +15,7 @@ class Calculator {
     }
 }
 
-public class MethodOverloading {
+public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println(calc.add(2, 3));        // Calls the first method
